e3895c02f8c5c055c5cb9b7a990cd544e124300e
kozueichikawa <wmwmwm1245@gmail.com>
2018-09-28 10:27:01 +0900

    コメント

---
+++ object/src/object/Robot.java
@@ -0,0 +1,18 @@
+package object;
+
+public class Robot {
+
+	
+	public static void main(String[]args){
+	 System.out.println("");
+	
+	
+	 Person aibo = new Person();
+		aibo.name ="aibo";
+
+		aibo.talk();
+		aibo.walk();
+		aibo.run();
+	
+	}
+}
