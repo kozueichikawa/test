65eba8a20c6cceb5e3d8899fb56927a257b93450
kozueichikawa <wmwmwm1245@gmail.com>
2018-10-19 17:55:13 +0900

    20181019

---
+++ template2/src/com/internousdev/tenplate2/action/HomeAction.java
@@ -0,0 +1,9 @@
+package com.internousdev.tenplate2.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HomeAction extends ActionSupport{
+     public String execute(){
+    	 return SUCCESS;
+     }
+}
